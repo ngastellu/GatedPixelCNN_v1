87466f977f905e467b6ced5d59e864d5b0aa94bc
InfluenceFunctional <mjakilgour@gmail.com>
2020-08-13 16:12:38 -0400

    Update create_batch_params.py

--- create_batch_params.py
+++ create_batch_params.py
@@ -4,14 +4,13 @@ import numpy as np
 import pickle
 
 params ={}
-layers = np.array((80, 60, 40)).astype('uint8')
-filters = np.array((60, 40, 20)).astype('uint16')
-temperature = np.array((0))
-n_runs= filters.size * layers.size * temperature.size
+layers = np.array((80, 60, 40)).astype('uint8') 
+filters = np.array((60, 40, 20)).astype('uint16') # try different combinations of layers and filters
+n_runs= filters.size * layers.size
 identity = np.ones(n_runs).astype('uint8')
-params['filters'] = [filters[i] for i in range(filters.size) for j in range(layers.size) for k in range(temperature.size)]
-params['layers'] = [layers[j] for i in range(filters.size) for j in range(layers.size) for k in range(temperature.size)]
-params['softmax_temperature'] = temperature * identity# [temperature[k] for i in range(filters.size) for j in range(layers.size) for k in range(temperature.size)]
+params['filters'] = [filters[i] for i in range(filters.size) for j in range(layers.size)]
+params['layers'] = [layers[j] for i in range(filters.size) for j in range(layers.size)]
+params['softmax_temperature'] = 0 * identity
 params['dataset_size'] = 40000 * identity
 params['model'] = 5 * identity
 params['filter_size'] = 3 * identity
@@ -26,8 +25,6 @@ params['train_margin'] = 1e-4 * identity
 params['average_over'] = 5 * identity
 params['outpaint_ratio'] = 4 * identity
 params['generation_type'] = 2 * identity
-params['noise'] = 0 * identity
-params['den_var'] = 0 * identity
 params['GPU'] = 1 * identity
 params['TB'] = 0 * identity
 
